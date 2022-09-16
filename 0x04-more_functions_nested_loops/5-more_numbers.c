0a90f10e88e21feb8f2d967c1aadf37d292cf840
Boluwatife Peter Oyedemi <boluwatifepeter4@gmail.com>
2022-09-16 21:02:54 +0100

    nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,15 @@
 
 void more_numbers(void)
 {
-  int i = 0, j;
+        int i = 0, j;
 
-  while (i < 10)
-    {
-      char *s = "01234567891011121314";
+        while (i < 10)
+        {
+                char *s = "01234567891011121314";
 
-      for (j = 0; j <= 19; j++)
-	_putchar(s[j]);
-      _putchar('\n')
-	i++;
-    }
+                for (j = 0; j <= 19; j++)
+	                      _putchar(s[j]);
+                _putchar('\n');
+	              i++;
+        }
 }
